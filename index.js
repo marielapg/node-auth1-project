1d2ccec88fa579ec064c11d8fda4c47ca139a0b5
marielapg <mpgonzales77@gmail.com>
2020-01-28 20:51:48 -0800

    mvp for day 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 5015;
+const PORT = process.env.PORT || 5030;
 server.listen(PORT, () => console.log(`\n** Running on port: ${PORT} **\n`));
